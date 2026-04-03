0f924417b955abf290f5d9d1b7168320c10ae3d7
Thomas Schuetz <thomas.schuetz@protos.de>
2026-04-03 17:48:01 +0200

    chore: add SharedAssemblyInfo.cs stub for builds outside the Lean mono-repo

---
+++ QuantConnect.InteractiveBrokersBrokerage/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,6 @@
+// Stub: replaces ../../Lean/Common/Properties/SharedAssemblyInfo.cs
+// which is not available outside the full Lean mono-repo checkout.
+using System.Reflection;
+
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
